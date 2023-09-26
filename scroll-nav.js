4dadfaab5c78f619efc4a30c4fa834f7757ab30f
so-ale <5363504+so-ale@users.noreply.github.com>
2023-09-26 17:25:19 +0530

    first commit

---
+++ scroll-nav.js
@@ -0,0 +1,30 @@
+
+// Toggle the .pa-fixed-header class when the user 
+// scroll 100px 
+
+window.onscroll = () => {scrollNavbar()};
+
+scrollNavbar = () => {
+    // Target elements
+    const navBar = document.getElementById("navBar");
+    const links = document.querySelectorAll("#navBar a");
+
+  if (document.documentElement.scrollTop > 100) {
+    navBar.classList.add("sl-fixed-header");
+
+    // Change the color of links on scroll
+    for (let i = 0; i < links.length; i++) {
+        const element = links[i];
+        element.classList.add('sl-text-toggle');
+    }
+
+  } else {
+    navBar.classList.remove("sl-fixed-header");
+    
+    // Change the color of links back to default
+    for (let i = 0; i < links.length; i++) {
+        const element = links[i];
+        element.classList.remove('sl-text-toggle');
+    }
+  }
+}
